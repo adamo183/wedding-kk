d9333e2e3ff5cede75f6363cf22605aea98d64d7
adamo183 <adam.majerski@op.pl>
2025-08-22 22:08:59 +0200

    fix

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,13 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { Observable } from "rxjs";
+import { catchError, Observable } from "rxjs";
 import { WeddingData } from "src/models/weddingData";
 
 @Injectable({ providedIn: 'root' })
 export class DataService {
   constructor(private http: HttpClient) {}
+  private endpointUrl = 'https://getweddingdata-5pdxbmxr2q-uc.a.run.app';
+  private localFile = 'data/info.json';
 
   loadData(): Observable<WeddingData> {
-    return this.http.get<WeddingData>('assets/info.json');
+    return this.http.get<WeddingData>(this.endpointUrl).pipe(
+      catchError(err => {
+        return this.http.get<WeddingData>(this.localFile);
+      })
+    );
   }
 }
